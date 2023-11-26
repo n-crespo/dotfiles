b0e81cb33110181c5afbdc00a0a6aa95142a84a3
nicolas <nicolasecrespo@gmail.com>
2023-11-26 02:28:08 -0500

    added powershell config

---
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,13 @@
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\tonybaloney.omp.json" | Invoke-Expression
+
+function Exit-Shell {
+    exit
+  }
+
+Set-Alias -Name q -Value Exit-Shell
+Set-PSReadlineOption -EditMode vi
+Import-Module -Name Terminal-Icons
+Import-Module PSReadLine
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -EditMode vi
